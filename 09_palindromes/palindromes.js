9d8813c9272945e18d0935af5a3224a9445cf251
Nick Cesare <ncesare@gmail.com>
2022-09-08 13:33:45 -0600

    Finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase()
+    let array = string.split('');
 
+    array = array.filter(isLetter);
+
+    let reverseArray = [];
+    for (char of array) {
+        reverseArray.unshift(char);
+    }
+
+    let reverseString = reverseArray.join('');
+    console.log(reverseString)
+
+    string = array.join('');
+    console.log(string);
+
+    if (string === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+function isLetter(char) {
+    return char.match(/[a-z]/i);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
