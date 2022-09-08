cfe5d0de58c742291a65f7fd4e9d16841d3dfa1b
Nick Cesare <ncesare@gmail.com>
2022-09-08 13:47:46 -0600

    Finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    position = parseInt(position);
 
+    if (position < 1) {
+        return 'OOPS';
+    }
+
+    fibonacciSequence = [1, 1];
+
+    for (let n = 2; n < position; n++) {
+        fibonacciSequence.push(fibonacciSequence[n-1] + fibonacciSequence[n-2]);
+    }
+
+    console.log(fibonacciSequence);
+
+    return fibonacciSequence[position-1];
 };
 
 // Do not edit below this line
