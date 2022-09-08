654f0649209a955af3b6f7956bc2b1174fcf0920
Nick Cesare <ncesare@gmail.com>
2022-09-08 12:52:55 -0600

    Finished exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  let total = 0;
+  for (let num of list) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  let total = 1;
+  for (let num of list) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1
+	for (; a > 0 ; a--) {
+    total *= a;
+  }
+  return total;
 };
 
 // Do not edit below this line
